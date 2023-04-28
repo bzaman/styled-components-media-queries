9b29fa5e57781006e014722233ae2a2ac8fd3d27
Badiuzzaman <folkgraphein@gmail.com>
2023-04-28 13:10:55 +0600

    clean up

--- index.js
+++ index.js
@@ -26,11 +26,3 @@ export default function mediaQueries (
     return `@media (max-width: ${( screens[breakpoints] || bpCustom ) - Number(0.02).toFixed(2)}px)`
   }
 }
-
-
-// mediaQueries() // @media (min-width: 768px) 
-// mediaQueries('max', 471) // @media (max-width: 470.98px)
-// mediaQueries('max', 'md') // @media (max-width: 767.98px)
-// mediaQueries('max', 'lg') // @media (max-width: 991.98px)
-// mediaQueries('min', 'lg')  // @media (min-width: 992px)
-// mediaQueries('min', 804) // @media (min-width: 804px)
